56bf009675f9e2def239ee924f19a93cc72885b8
im <poson_hemp@yahoo.co.jp>
2020-05-06 18:07:05 +0900

    add fields & update types

--- psql_cmd/create_tb.sql
+++ psql_cmd/create_tb.sql
@@ -1,8 +1,25 @@
 -- create Table command
-/*
-確認用
-*/
 CREATE TABLE posts (
-  title varchar(255),
-  body  text
+  id       serial PRIMARY KEY,
+  title    varchar(255) NOT NULL,
+  body     text CHECK(length(body) > 2),
+  is_draft boolean DEFAULT true,
+  posted   timestamp DEFAULT statement_timestamp()
 );
+
+/* Results
+
+psql_blogapp=# \d posts
+                                       Table "public.posts"
+  Column  |            Type             | Collation | Nullable |              Default              
+----------+-----------------------------+-----------+----------+-----------------------------------
+ id       | integer                     |           | not null | nextval('posts_id_seq'::regclass)
+ title    | character varying(255)      |           | not null | 
+ body     | text                        |           |          | 
+ is_draft | boolean                     |           |          | true
+ posted   | timestamp without time zone |           |          | statement_timestamp()
+Indexes:
+    "posts_pkey" PRIMARY KEY, btree (id)
+Check constraints:
+    "posts_body_check" CHECK (length(body) > 2)
+*/
