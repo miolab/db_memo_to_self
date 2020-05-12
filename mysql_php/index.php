99b257f4d1b1dd27cb2b5871c092c64d5d1f13a4
im <poson_hemp@yahoo.co.jp>
2020-05-12 09:34:41 +0900

    file rename

---
+++ mysql_php/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MySQL with PHP (READ)</title>
+</head>
+
+<body>
+  <h1>MySQL with PHP (READ)</h1>
+  <table>
+
+    <?php
+
+    // Connect DB
+
+    use FFI\Exception;
+
+    $db = "mydb_test";
+    $user = "root";
+    $password = "";
+    $charset = "utf8mb4";
+    $dsn = "mysql:dbname={$db};host=127.0.0.1;charset={$charset}";
+
+    try {
+      $dbh = new PDO($dsn, $user, $password);
+
+      // Handling DB
+      $sql = "select * from menu";
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute();
+
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        print "<tr>\n";
+        print "\t<td>{$row['name']}</td>\n";
+        print "\t<td>{$row['score']}</td>\n";
+        print "\t<td>{$row['stars']}</td>\n";
+        print "</tr>\n";
+      }
+
+      // Disconnect DB
+      $dbh = null;
+    } catch (Exception $e) {
+      print "ただいま障害によりご迷惑をおかけしております";
+    }
+    ?>
+
+  </table>
+</body>
+
+</html>
